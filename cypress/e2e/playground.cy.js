15c607245ff6aa1bf176e5646191d8ddfbad71b3
felipevitalinobarra <felipevitalino96@gmail.com>
2025-09-29 09:42:35 -0300

    Lesson 2: Learning about visiting web pages

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -1,4 +1,8 @@
 describe('Cypress Playground', () => {
+  beforeEach(() => {
+    cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+  })
+
   it('shows a promotional banner', () => {
     // Implementação do caso de teste
   })
