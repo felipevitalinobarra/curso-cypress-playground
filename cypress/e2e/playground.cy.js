ec5f73630c8fc81b542563c0f20cfeebb468e30e
felipevitalinobarra <felipevitalino96@gmail.com>
2025-09-29 12:35:23 -0300

    Lesson 3: Clicking on elements

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -4,6 +4,15 @@ describe('Cypress Playground', () => {
   })
 
   it('shows a promotional banner', () => {
-    // Implementação do caso de teste
+    cy.get('#promotional-banner').should('be.visible')
+  })
+
+  it('clicks the Subscribe button and shows a success message', () => {
+    cy.contains('button', 'Subscribe').click()
+
+    cy.contains(
+      '#success',
+      "You've been successfully subscribed to our newsletter."
+    ).should('be.visible')
   })
 })
