5b88fa2eedecdaa2e71420218a1bf829ed4d87fb
felipevitalinobarra <felipevitalino96@gmail.com>
2025-10-01 10:05:33 -0300

    Lesson 5: Checking and unchecking checkboxes

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -21,4 +21,15 @@ describe('Cypress Playground', () => {
     
     cy.contains('#signature', 'Felipe Barra').should('be.visible')
   })
+
+  it('types in the signature field, checks the checkbox to see the preview, then unchecks it', () => {
+    cy.get('#signature-textarea-with-checkbox').type('Felipe Barra')
+    cy.get('#signature-checkbox').check().should('be.checked')
+
+    cy.contains('#signature-triggered-by-check', 'Felipe Barra').should('be.visible')
+
+    cy.get('#signature-checkbox').uncheck().should('not.be.checked')
+    
+    cy.contains('#signature-triggered-by-check', 'Felipe Barra').should('not.exist')
+  })
 })
