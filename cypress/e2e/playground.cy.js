ed560d7e767afd7fea6c91dbd8af97451ee3457a
felipevitalinobarra <felipevitalino96@gmail.com>
2025-10-05 12:42:16 -0300

    Lesson 10: Intercepting and waiting for requests

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -81,4 +81,19 @@ describe('Cypress Playground', () => {
     cy.contains('p', 'The following file has been selected for upload: example.json')
       .should('be.visible')
   })
+
+  it('clicks a button and triggers a request', () => {
+    cy.intercept('GET', 'https://jsonplaceholder.typicode.com/todos/1')
+      .as('getTodo')
+
+    cy.contains('button', 'Get TODO').click()
+    cy.wait('@getTodo')
+      .its('response.statusCode')
+      .should('be.equal', 200)
+
+    cy.contains('li', 'TODO ID: 1').should('be.visible')
+    cy.contains('li', 'Title: delectus aut autem').should('be.visible')
+    cy.contains('li', 'Completed: false').should('be.visible')
+    cy.contains('li', 'User ID: 1').should('be.visible')
+  })
 })
